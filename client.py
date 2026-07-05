b148a2ed5c817dfefc3e8fc7a206b50d2fc30fc6
Logika Student <logika220225@gmail.com>
2026-07-05 17:31:35 +0300

    змінив позицію текста рахунку

--- client.py
+++ client.py
@@ -112,8 +112,8 @@ while True:
         draw.rect(screen, (0, 255, 0), (20, game_state['paddles']['0'], 20, 100))
         draw.rect(screen, (255, 0, 255), (WIDTH - 40, game_state['paddles']['1'], 20, 100))
         screen.blit(ball_image, (game_state['ball']['x'] - 10, game_state['ball']['y'] - 10))
-        score_text = font_main.render(f"{game_state['scores'][0]}           :          {game_state['scores'][1]}", True, (255, 255, 255))
-        screen.blit(score_text, (WIDTH // 2 -50, 20))
+        score_text = font_main.render(f"{game_state['scores'][0]}                       {game_state['scores'][1]}", True, (255, 255, 255))
+        screen.blit(score_text, (WIDTH // 2 -90, 17))
 
         if game_state['sound_event']:
             if game_state['sound_event'] == 'wall_hit':
